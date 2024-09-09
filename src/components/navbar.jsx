2dc1512849a27d7b769553e17fb795e7d5d0090c
Dylan Guidry <dylanguidry@Dylans-Laptop.attlocal.net>
2024-09-09 14:12:35 -0500

    adding some images and updated the looks of the homepage slightly while also working on the api call

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import './navbar.css';
+import logo from '../images/1337497_game_go_play_pikachu_pokemon_icon.png'
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-danger shadow-sm custom-gradient">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">MyApp</Link>
+        <Link className="navbar-brand py-2 px-3" to="/">
+          <img src={logo} alt="Logo" style={{ height: '50px' }} /> 
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
